0e04a969aaa76e3651a963a80b9cc8c6f447e9e2
doublemc <ciupa.michal@gmail.com>
2017-02-25 17:45:43 +0100

    Initial commit

---
+++ src/main/java/com/michalc94/BookrestApplication.java
@@ -0,0 +1,12 @@
+package com.michalc94;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookrestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookrestApplication.class, args);
+	}
+}
